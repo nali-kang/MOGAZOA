ccd8159d3b11891153b0aa0236bae45255d878c6
nali-kang <softnali92@gmail.com>
2024-05-22 23:11:09 +0900

    Fix: test 페이지 작성

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,24 +1,57 @@
 'use client';
 
-import Dropdown, { Option } from '@/Components/Commons/Dropdown';
+import { Dropdown, DropdownSearch, Option } from '@/Components/Commons/Dropdown/DropdownComponent';
 import { useState } from 'react';
 
 function TestPage() {
   const [selectOption, setSelectOption] = useState<string>('option');
+  const [search, setSearch] = useState<string>('');
   const option: Option[] = [
     { label: '옵션1', value: 'option' },
     { label: '옵션2', value: 'option1' },
     { label: '옵션3', value: 'option2' },
     { label: '옵션4', value: 'option3' },
+    { label: '옵션5', value: 'option4' },
+    { label: '옵션6', value: 'option5' },
+    { label: '옵션7', value: 'option6' },
+    { label: '옵션8', value: 'option7' },
+    { label: '옵션9', value: 'option8' },
+    { label: '옵션10', value: 'option9' },
   ];
   return (
-    <Dropdown
-      option={option}
-      value={selectOption}
-      onChange={(value: any) => {
-        setSelectOption(value);
-      }}
-    />
+    <div className="flex gap-2">
+      <Dropdown
+        option={option}
+        value={selectOption}
+        onChange={(value: any) => {
+          setSelectOption(value);
+        }}
+      />
+      <DropdownSearch
+        option={option}
+        value={selectOption}
+        onChange={(value: any) => {
+          setSelectOption(value);
+        }}
+        type="tag_first"
+      />
+      <DropdownSearch
+        option={option}
+        value={search}
+        onChange={(value: any) => {
+          setSearch(value);
+        }}
+        type="tag_second"
+      />
+      <DropdownSearch
+        option={option}
+        value={search}
+        onChange={(value: any) => {
+          setSearch(value);
+        }}
+        type="search"
+      />
+    </div>
   );
 }
 
