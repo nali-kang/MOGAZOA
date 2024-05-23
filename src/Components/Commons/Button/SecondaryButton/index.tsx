97f4304d7e18cffff958f7f0d7c1c7a957f39367
gun7741 <gun7741@naver.com>
2024-05-23 16:53:07 +0900

    feat : SecondaryButton 컴포넌트 버튼역할 props 추가

--- src/Components/Commons/Button/SecondaryButton/index.tsx
+++ src/Components/Commons/Button/SecondaryButton/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ButtonProps } from '@/Types/ButtonProps';
+import { useState } from 'react';
 
 const btnSizeClassnames: Record<ButtonProps['btnSize'], string> = {
   'btn-640': 'w-[335px] h-[50px] md:w-[440px] md:h-[55px] lg:w-[640px] lg:h-[65px] text-base lg:text-lg',
@@ -11,22 +12,55 @@ const btnSizeClassnames: Record<ButtonProps['btnSize'], string> = {
   'btn-185': 'w-[335px] h-[50px] md:w-[140px] md:h-[55px] lg:w-[185px] lg:h-[65px] text-base lg:text-lg',
 };
 
-export default function SecondaryButton({ children, handleClick, isDisabled = false, btnSize }: ButtonProps) {
+interface SecondaryButtonProps extends ButtonProps {
+  btnRole: 'submit' | 'choose';
+  // 일반 버튼 submit, 비교 시 사용하는 버튼 choose
+}
+
+export default function SecondaryButton({
+  children,
+  handleClick,
+  isDisabled = false,
+  btnSize,
+  btnRole,
+}: SecondaryButtonProps) {
+  const [isBtnActive, setIsBtnActive] = useState(false);
+
   const sizeClass = btnSizeClassnames[btnSize];
 
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsBtnActive(!isBtnActive);
+    handleClick(e);
+    console.log(isBtnActive);
+  };
+
+  if (btnRole === 'submit') {
+    return (
+      <div>
+        <button
+          type="button"
+          onClick={handleClick}
+          disabled={isDisabled}
+          className={`flex justify-center px-24 py-288 items-center rounded-lg bg-transparent  border ${isDisabled ? 'text-gray1 border-black3' : 'border-blue'} ${sizeClass}`}
+        >
+          <p
+            className={`${isDisabled ? 'text-gray1' : 'bg-gradient-to-r from-blue to-indigo text-transparent bg-clip-text'}`}
+          >
+            {children}
+          </p>
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
       <button
         type="button"
-        onClick={handleClick}
+        onClick={handleButtonClick}
         disabled={isDisabled}
-        className={`flex justify-center px-24 py-288 items-center rounded-lg bg-transparent  border ${isDisabled ? 'text-gray1 border-black3' : 'border-blue'} ${sizeClass}`}
+        className={`flex justify-center px-24 py-288 items-center rounded-lg bg-transparent  border ${isBtnActive ? 'border-pink' : 'text-gray1 border-black3'} ${sizeClass}`}
       >
-        <p
-          className={`${isDisabled ? 'text-gray1' : 'bg-gradient-to-r from-blue to-indigo text-transparent bg-clip-text'}`}
-        >
-          {children}
-        </p>
+        <p className={`${isBtnActive ? 'text-pink' : 'text-gray1'}`}>{children}</p>
       </button>
     </div>
   );
