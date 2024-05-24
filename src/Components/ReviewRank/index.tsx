132684d130798733067de56309d270b4b5caf658
gun7741 <gun7741@naver.com>
2024-05-24 17:19:02 +0900

    feat : 리뷰 랭킹 컴포넌트 제작중..

--- src/Components/ReviewRank/index.tsx
+++ src/Components/ReviewRank/index.tsx
@@ -1,3 +1,13 @@
+import RankProfile from './RankProfile';
+import mockData from './mockData.json';
+
 export default function ReveiwRank() {
-  return <div>TODO : Review Ranking</div>;
+  return (
+    <div>
+      <p className="text-white mb-[20px]">리뷰어 랭킹</p>
+      {mockData.map((profile) => (
+        <RankProfile key={profile.id} profile={profile} />
+      ))}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ReveiwRank from '@/Components/ReviewRank';
+
 export default function Home() {
   return (
     <div>
       <div className="bg-white w-full h-[70px] md:h-[80px] lg:h-[100px]">header</div>
+      <ReveiwRank />
     </div>
   );
 }
