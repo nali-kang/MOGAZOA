d5d5d2b4c3972933e53b95442c124e8446941c1c
gun7741 <gun7741@naver.com>
2024-05-25 14:42:36 +0900

    refactor : 공통 버튼 컴포넌트 리팩토링(코드리뷰반영)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import Button from '@/Components/Commons/Button';
+
 export default function Home() {
-  return <div>homepage</div>;
+  return (
+    <div>
+      <Button
+        color="secondary"
+        onClick={() => {
+          console.log('clcik');
+        }}
+        // disabled
+        // disabled={false}
+        className="w-[100px] h-[100px]"
+        variant="clickBased"
+      >
+        testt
+      </Button>
+    </div>
+  );
 }
