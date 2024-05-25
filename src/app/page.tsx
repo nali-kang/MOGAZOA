a67626f6330ff7a058de79a314d4d8b9fcbb2103
gun7741 <gun7741@naver.com>
2024-05-25 14:45:15 +0900

    test : testcode삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-'use client';
-
-import Button from '@/Components/Commons/Button';
-
 export default function Home() {
-  return (
-    <div>
-      <Button
-        color="secondary"
-        onClick={() => {
-          console.log('clcik');
-        }}
-        // disabled
-        // disabled={false}
-        className="w-[100px] h-[100px]"
-        variant="clickBased"
-      >
-        testt
-      </Button>
-    </div>
-  );
+  return <div>homepage</div>;
 }
