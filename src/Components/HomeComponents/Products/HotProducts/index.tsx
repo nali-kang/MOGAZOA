acc32badc493d30ddf5d5f1b43a229e1e8a28541
gun7741 <gun7741@naver.com>
2024-05-25 13:31:28 +0900

    Design : product 디자인 수정중,,

--- src/Components/HomeComponents/Products/HotProducts/index.tsx
+++ src/Components/HomeComponents/Products/HotProducts/index.tsx
@@ -7,7 +7,7 @@ export default function HotProducts() {
     .sort((a: ProductType, b: ProductType) => b.favoriteCount - a.favoriteCount)
     .slice(0, 6);
   return (
-    <div className="grid grid-cols-2 gap-[15px]">
+    <div className="grid grid-cols-2 gap-[15px] lg:grid-cols-3 lg:gap-[20px]">
       {sortedHotProductData.map((product: ProductType) => (
         <Products key={product.id} product={product} />
       ))}
