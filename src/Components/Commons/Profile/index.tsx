80a1e1b2a2f1d2c6619dd92a817ce21788704f8c
gun7741 <gun7741@naver.com>
2024-05-29 18:09:36 +0900

    feat : profile 컴포넌트 프롭스 추가

--- src/Components/Commons/Profile/index.tsx
+++ src/Components/Commons/Profile/index.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 interface ProfileProps {
   profileUrl?: string;
+  className?: string;
 }
 
-export default function Profile({ profileUrl }: ProfileProps) {
+export default function Profile({ profileUrl, className = 'w-[36px] h-[36px] lg:w-[42px] lg:h-[42px]' }: ProfileProps) {
   const InitialProfile = '/Images/img-profile1.svg';
+
   return (
-    <div className="relative w-[36px] h-[36px] lg:w-[42px] lg:h-[42px]">
+    <div className={`relative ${className}`}>
       <Image src={profileUrl || InitialProfile} alt="profileImage" fill priority />
     </div>
   );
