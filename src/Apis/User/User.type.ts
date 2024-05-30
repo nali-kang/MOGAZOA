06f8f79d6300a10716f9e334c116867ad828ca03
minho <kasamin98@naver.com>
2024-05-31 02:22:07 +0900

    feat: User Service type 기초 설정(api)

---
+++ src/Apis/User/User.type.ts
@@ -0,0 +1,73 @@
+export type PatchUserProps = {
+  description: string;
+  nickname: string;
+  image: string | null;
+};
+export type UserId = {
+  userId: number;
+};
+export type RankingInfo = {
+  id: number;
+  nickname: string;
+  description: string;
+  image: string | null;
+  createdAt: string;
+  updatedAt: string;
+  teamId: string;
+  followersCount: number;
+  reviewCount: number;
+};
+export type GetUserRanking = RankingInfo[];
+export type UserInformationRes = {
+  id: number;
+  nickname: string;
+  description: string;
+  image?: string | null;
+  createdAt: string;
+  updatedAt: string;
+  teamId: number;
+  isFollowing: boolean;
+  followersCount: number;
+  followeesCount: number;
+  reviewCount: number;
+  averageRating: number;
+  mostFavoriteCategory?: string;
+};
+export type PatchUserMeRes = {
+  id: number;
+  nickname: string;
+  description: string;
+  image: string | null;
+  createdAt: string;
+  updatedAt: string;
+  teamId: number;
+};
+export type Follow = {
+  updatedAt: Date;
+  createdAt: Date;
+  teamId: string;
+  image: string | null;
+  description: string;
+  nickname: string;
+  id: number;
+};
+export type GetUserFollow = {
+  list: Follow[];
+  nextCursor: null;
+};
+export type Product = {
+  updatedAt: Date;
+  createdAt: Date;
+  writerId: number;
+  categoryId: number;
+  favoriteCount: number;
+  reviewCount: number;
+  rating: number;
+  image: string | null;
+  name: string;
+  id: number;
+};
+export type GetProduct = {
+  nextCursor: number;
+  list: Product[];
+};
