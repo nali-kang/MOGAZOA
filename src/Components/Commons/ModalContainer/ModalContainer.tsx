f9e2116798f5398052b2789ee822746ad6a7eeea
minho <kasamin98@naver.com>
2024-06-06 00:41:01 +0900

    feat: ModalContainer.tsx 생성

---
+++ src/Components/Commons/ModalContainer/ModalContainer.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import TestModal from '@/Components/Modal/TestModal';
+import { ModalSetterContext, ModalStateContext } from '@/Context/ModalContext';
+import { FunctionComponent, useContext } from 'react';
+import Modal from 'react-modal';
+
+const modalStyles: ReactModal.Styles = {
+  overlay: {
+    backgroundColor: ' rgba(0, 0, 0, 0.70)',
+    width: '100%',
+    height: '100vh',
+    zIndex: '10',
+    position: 'fixed',
+    top: '0',
+    left: '0',
+  },
+  content: {
+    borderRadius: '16px',
+    zIndex: '150',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+    justifyContent: 'center',
+    overflow: 'auto',
+    border: '0px',
+  },
+};
+
+if (typeof window !== 'undefined') {
+  Modal.setAppElement(document.body);
+}
+type ModalComponents = {
+  [key: string]: FunctionComponent<any>;
+};
+
+const MODAL_COMPONENTS: ModalComponents = {
+  // test: TestModal, 이런식으로 추가
+};
+
+export default function ModalContainer() {
+  const modalState = useContext(ModalStateContext);
+  const setModalState = useContext(ModalSetterContext);
+
+  if (!modalState.type) {
+    return null;
+  }
+
+  const Contents = MODAL_COMPONENTS[modalState.type];
+
+  return (
+    <Modal
+      className=" bg-bgblack w-[335px] h-[518px] md:w-[590px] md:h-[642px] xl:w-[620px] xl:h-[698px] "
+      isOpen={modalState.isOpen}
+      style={modalStyles}
+      onRequestClose={() => {
+        setModalState(false);
+      }}
+    >
+      <Contents />
+    </Modal>
+  );
+}
