f56871f83d6d534ccdc291f4cae852054adc4d9c
minho <kasamin98@naver.com>
2024-06-17 12:35:57 +0900

    feat: ProductDetailContainer.tsx생성

---
+++ src/Components/ProductDetail/ProductDetailContainer.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useGetProductDetail } from '@/Apis/Product/useProduct.Service';
+import Detail from './Detail';
+import ProductStat from './ProductStat';
+import ProductReview from './Review';
+import { useParams } from 'next/navigation';
+
+export default function ProductDetailContainer() {
+  const { productId } = useParams() as { productId: string };
+  const numberProductId = parseInt(productId, 10);
+  const { data } = useGetProductDetail(numberProductId);
+
+  // const token = Cookies.get('token');
+  // console.log(token);
+
+  // console.log(data);
+  return (
+    <div className=" text-white flex-col justify-center">
+      <Detail data={data} />
+      <div className=" font-semibold text-lg p-5 mt-4 xl:text-2xl">상품 통계</div>
+      <ProductStat />
+      <ProductReview productId={numberProductId} />
+    </div>
+  );
+}
