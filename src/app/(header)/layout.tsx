6c4173c5e53a944b7a88ef1a4ade9e08e9fa8eca
gun7741 <gun7741@naver.com>
2024-06-22 12:29:00 +0900

    fix : hydration 오류 수정

--- src/app/(header)/layout.tsx
+++ src/app/(header)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { SearchProvider } from '../SearchContext';
 import { CategoryProvider } from '../CategoryContext';
 import ClientNavigationBar from '@/Components/Commons/NavigationBar/ClientNavigationBar';
