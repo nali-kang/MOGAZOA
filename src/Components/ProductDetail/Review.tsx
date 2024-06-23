9d533292b7ef5bd3c1ead5f374b12531ad8f4552
minho <kasamin98@naver.com>
2024-06-24 05:19:13 +0900

    feat: 리뷰 정렬, 좋아요버튼 기능 추가

--- src/Components/ProductDetail/Review.tsx
+++ src/Components/ProductDetail/Review.tsx
@@ -1,33 +1,56 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { Dropdown, Option } from '../Commons/Dropdown/DropdownComponent';
 import { useGetProductReviewList } from '@/Apis/Product/useProduct.Service';
 import { Rating } from 'react-simple-star-rating';
 import { useRouter } from 'next/navigation';
-import { useDeleteReview } from '@/Apis/Review/useReview.Service';
+import { useDeleteReview, useDeleteReviewLike, usePostReviewLike } from '@/Apis/Review/useReview.Service';
+import getUserInfoFromToken from '@/Utils/getUserInfoFromToken';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function ProductReview({ productId }: any) {
   const router = useRouter();
-  const [sorting, setSorting] = useState<'latest' | 'upstar' | 'downstar' | 'like'>('latest');
-  const { data } = useGetProductReviewList(productId);
-  const defaultUserImage = '/images/apple-watch.svg';
-  // TODO: 기본 이미지 설정
-
+  const [sorting, setSorting] = useState<'recent' | 'ratingDesc' | 'ratingAsc' | 'likeCount'>('recent');
+  const { data } = useGetProductReviewList(productId, sorting);
   const sort: Option[] = [
-    { label: '최신순', value: 'latest' },
-    { label: '별점 높은순', value: 'upstar' },
-    { label: '별점 낮은순', value: 'downstar' },
-    { label: '좋아요순', value: 'like' },
+    { label: '최신순', value: 'recent' },
+    { label: '별점 높은순', value: 'ratingDesc' },
+    { label: '별점 낮은순', value: 'ratingAsc' },
+    { label: '좋아요순', value: 'likeCount' },
   ];
-  // console.log(data);
-  // API
+  // TODO: 유저 프로필 기본 이미지 설정
+  const defaultUserImage = '/images/apple-watch.svg';
+
+  // 리뷰 삭제
   const deleteReview = useDeleteReview(0);
-  function handleDeleteButton(e: React.MouseEvent<HTMLImageElement>) {
+  function handleDeleteButton(e: MouseEvent<HTMLImageElement>) {
     deleteReview.mutate(e.currentTarget.id);
     window.location.reload();
   }
+  // 내 아이디 확인
+  const [userId, setUserId] = useState(0);
+  const token = getUserInfoFromToken();
+
+  // 좋아요버튼
+  const queryClient = useQueryClient();
+  const postReviewLike = usePostReviewLike(0);
+  const deleteReviewLike = useDeleteReviewLike(0);
+  async function handleLikeClick(e: MouseEvent<HTMLButtonElement>) {
+    await postReviewLike.mutate(e.currentTarget.id);
+    await queryClient.invalidateQueries();
+    await queryClient.refetchQueries();
+  }
+  async function handleUnlikeClick(e: MouseEvent<HTMLButtonElement>) {
+    await deleteReviewLike.mutate(e.currentTarget.id);
+    await queryClient.invalidateQueries();
+    await queryClient.refetchQueries();
+  }
+
+  useEffect(() => {
+    setUserId(token.id);
+  }, []);
   return (
     <div className="flex-col ">
       <div className="w-[375px] md:w-[726px] xl:w-[980px] flex justify-between items-center mb-7">
@@ -46,15 +69,19 @@ export default function ProductReview({ productId }: any) {
           key={review.id}
           className="relative w-[335px] md:w-[684px] xl:w-[940px] bg-scblack rounded-xl border border-zinc-700 justify-center mx-auto p-5 md:flex mb-3"
         >
-          <Image
-            id={review.id}
-            src="/Icons/close-icon.svg"
-            alt="close-modal"
-            className="w-4 h-4 absolute right-4 top-4 hover:cursor-pointer"
-            width={10}
-            height={10}
-            onClick={handleDeleteButton}
-          />
+          {review.userId === userId ? (
+            <Image
+              id={review.id}
+              src="/Icons/close-icon.svg"
+              alt="close-modal"
+              className="w-4 h-4 absolute right-4 top-4 hover:cursor-pointer"
+              width={10}
+              height={10}
+              onClick={handleDeleteButton}
+            />
+          ) : (
+            <div />
+          )}
           <div className="justify-start items-start gap-2.5 flex">
             <Image
               src={review.user.image || defaultUserImage}
@@ -96,16 +123,43 @@ export default function ProductReview({ productId }: any) {
                 {review.updatedAt.substr(0, 10)}
               </div>
               <div className="flex-col justify-start items-start gap-2.5 inline-flex">
-                <button
-                  type="button"
-                  className="px-2.5 py-1.5 bg-zinc-800 rounded-[100px] border border-zinc-700 justify-center items-center gap-[5px] flex"
-                >
-                  <Image src="/Icons/like-icon.svg" alt="따봉" width={14} height={14} className="md:w-5 md:h-5" />
-                  <div className=" text-xs bg-gradient font-normal font-['Pretendard'] bg-clip-text text-transparent">
-                    {review.likeCount}
-                  </div>
-                  {/* TODO: 내가 좋아요 표시하면 색칠,아니면 빈따봉 */}
-                </button>
+                {review.isLiked ? (
+                  <button
+                    id={review.id}
+                    onClick={handleUnlikeClick}
+                    type="button"
+                    className="px-2.5 py-1.5 bg-zinc-800 rounded-[100px] border border-zinc-700 justify-center items-center gap-[5px] flex"
+                  >
+                    <Image
+                      src="/Icons/like-icon.svg"
+                      alt="좋아요활성"
+                      width={14}
+                      height={14}
+                      className="md:w-5 md:h-5"
+                    />
+                    <div className=" text-xs bg-gradient font-normal font-['Pretendard'] bg-clip-text text-transparent">
+                      {review.likeCount}
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    id={review.id}
+                    type="button"
+                    className="px-2.5 py-1.5 bg-zinc-800 rounded-[100px] border border-zinc-700 justify-center items-center gap-[5px] flex"
+                    onClick={handleLikeClick}
+                  >
+                    <Image
+                      src="/Icons/unlike-icon.svg"
+                      alt="비활성좋아요"
+                      width={14}
+                      height={14}
+                      className="md:w-5 md:h-5"
+                    />
+                    <div className=" text-xs bg-slate-300 font-normal font-['Pretendard'] bg-clip-text text-transparent">
+                      {review.likeCount}
+                    </div>
+                  </button>
+                )}
               </div>
             </div>
           </div>
